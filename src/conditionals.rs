a23f27a028549b88090c2f2886ca9e631219c930
texhnolainx <t3xhnolainx@gmail.com>
2020-12-04 15:37:01 +0100

    Conditionals change

--- src/conditionals.rs
+++ src/conditionals.rs
@@ -1,9 +1,10 @@
 pub fn run () {
     let age: u8 = 18;
     let check_id: bool = false;
+    let knows_person_of_age = true;
 
     // If/Else
-    if age >= 21 && check_id {
+    if age >= 21 && check_id || knows_person_of_age {
         println!("Bartender: What would you like to drink?");
     } else if age < 21 && check_id {
         println!("Bartender: Sorry, you have to leave!");
